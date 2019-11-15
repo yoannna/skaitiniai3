8f58cc9f1f7e94ffc8aa11e39f118a8cf32f26e0
Joana <olga.smitaite@mif.stud.vu.lt>
2019-11-15 14:20:21 +0200

    Update 3-6.py

--- 3-6.py
+++ 3-6.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import splev, splrep
+plt.style.use('seaborn-whitegrid')
 
-start = 0.0
-stop = 40.0
-n = 20
+start = -2.0
+stop = 4.0
+n = 10
 step = (stop - start) / n
 x = []
 y = []
@@ -21,9 +21,9 @@ def getE(index, e = 0):
     eq2 = [x[index + 1]*x[index + 1], x[index + 1], 1]
     eq3 = [2*x[index],  1, 0]
     eq = np.array([y[index], y[index + 1], e])
-    sol = np.linalg.solve(np.array([eq1, eq2, eq3]), eq)
-    e1 = 2*sol[0]*x[index+1] + sol[1]
-    return sol, e1
+    coef = np.linalg.solve(np.array([eq1, eq2, eq3]), eq)
+    e1 = 2*coef[0]*x[index+1] + coef[1]
+    return coef, e1
 
 split()
 
@@ -31,17 +31,14 @@ fig = plt.figure()
 ax = plt.axes()
 ax.set_facecolor('xkcd:black')
 
-xPlot = np.linspace(start, stop, 1000)
-ax.plot(xPlot, f(xPlot))
+xPlt = np.linspace(start, stop, 1000)
+ax.plot(xPlt, f(xPlt))
 
-sol = []
 e = 0
 for i in range(0, n):
-    currentSol, e = getE(i, e)
-    sol.append(currentSol)
-
-    xPlot = np.linspace(start + step*i, start + step*(i+1), 1000)
-    fCurrent = lambda x: currentSol[0]*x*x + currentSol[1]*x + currentSol[2]
-    ax.plot(xPlot, fCurrent(xPlot))
+    coef, e = getE(i, e)
+    xPlt = np.linspace(start + step*i, start + step*(i+1), 1000)
+    fi = lambda x: coef[0]*x*x + coef[1]*x + coef[2]
+    ax.plot(xPlt, fi(xPlt))
 
 plt.show()
